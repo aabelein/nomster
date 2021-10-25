572e04fc9d2bf1aa776b18311670cbb1a8143f95
Ariel Abelein <arielabelein@gmail.com>
2021-10-25 20:52:40 +0000

    links to entries

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,10 +14,14 @@ class PlacesController < ApplicationController
    	redirect_to root_path
    end
 
+   def show
+   	@place = Place.find_by(name: params[:id])
+   end
+
    private
 
    def place_params
-   	params.require(:place).permit(:name, :description, :address)
+   	params.require(:place).permit(:name, :description, :address, :id)
    end
 
 end
