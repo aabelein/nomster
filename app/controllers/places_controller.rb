31e442c57058ba309583dcac6bd5b2489bd0c212
Ariel Abelein <arielabelein@gmail.com>
2021-10-27 19:26:58 +0000

    validating user input

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,9 +10,13 @@ class PlacesController < ApplicationController
    end
 
    def create
-   	current_user.places.create(place_params)
+   	@place = current_user.places.create(place_params)
+    if @place.valid?
    	redirect_to root_path
+   else
+    render :new, status: :unprocessable_entity
    end
+end
 
    def show
    	@place = Place.find_by(id: params[:id])
@@ -33,15 +37,19 @@ class PlacesController < ApplicationController
     end
 
    	@place.update_attributes(place_params)
+    if @place.valid?
    	redirect_to root_path
+   else
+    render :edit, status: :unprocessable_entity
    end
+end
 
    def destroy
    	@place = Place.find(params[:id])
     if @place.user != current_user
         return render plain: 'Not Allowed', status: :forbidden
     end
-    
+
    	@place.destroy
    	redirect_to root_path
    end
