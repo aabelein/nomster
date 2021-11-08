189b3ca271f8b0178ed33b0d13b8831419b40a5e
Ariel Abelein <arielabelein@gmail.com>
2021-11-08 20:31:50 +0000

    sending emails

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,8 +22,6 @@ end
    	@place = Place.find_by(id: params[:id])
     @comment = Comment.new
     @photo = Photo.new
-    puts "SENDGRID_USERNAME: #{ENV['SENDGRID_USERNAME']}"
-    puts "SENDGRID_PASSWORD: #{ENV['SENDGRID_PASSWORD']}"
    end
 
    def edit
